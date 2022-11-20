6e35582adab20502fb392f8ee9194105535d0e66
nyetes <nitesh.mahar1208@gmail.com>
2022-11-20 21:30:41 +0545

    connectdb

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,16 @@
-const express = require ('express')
-const {register} = require('../controllers/auth')
-const router = express.Router()
+const express = require('express')
 
+const router = express.Router()
 const {
-    getAllJobs,
-    getJob, 
-    createJob, 
-    updateJob,
-    deleteJob
-    
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
 } = require('../controllers/jobs')
 
 router.route('/').post(createJob).get(getAllJobs)
+
 router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+
+module.exports = router
